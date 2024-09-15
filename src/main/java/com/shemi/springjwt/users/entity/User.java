c31178ab0b139acf483c321fd8538f5c99dac8a9
Victor Shemi <victor.s@nathandigital.com>
2024-09-15 11:04:59 +0300

    first commit

---
+++ src/main/java/com/shemi/springjwt/users/entity/User.java
@@ -0,0 +1,10 @@
+package com.shemi.springjwt.users.entity;
+
+
+public class User {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
